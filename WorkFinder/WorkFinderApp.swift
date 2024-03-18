a95b1845db21c2d857938193e40dcb30eeb4e751
Alexandra Makey <aliaksandra.makey@gmail.com>
2024-03-18 21:59:11 +0400

    Initial Commit

---
+++ WorkFinder/WorkFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkFinderApp.swift
+//  WorkFinder
+//
+//  Created by Александра Макей on 18.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct WorkFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
